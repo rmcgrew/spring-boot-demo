4c620b00ad2c77d169a72193e61429d9625cef1f
Ryan McGrew <ryanmcgrew@gmail.com>
2016-06-06 14:30:21 +0200

    Initial commit with skeleton Spring Boot Application, Gradle Config and Dockerfile

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,25 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.bind.RelaxedPropertyResolver;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by rmcgrew on 6/6/16.
+ */
+@SpringBootApplication
+@RestController
+public class Application {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello Docker World";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
